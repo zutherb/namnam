bc87c8dc025f51f8e4491feff55532f7762de57f
Bernd Zuther <bernd.zuther@comsysto.com>
2013-07-07 19:21:30 +0200

    edit query example

--- src/test/scala/org/namnam/dsl/QueryExamples.scala
+++ src/test/scala/org/namnam/dsl/QueryExamples.scala
@@ -4,10 +4,10 @@ object QueryExamples extends QueryBuilder {
 
   def main(args: Array[String]) {
     println(
-      create().connection().to("hdfs://192.2.2.1/test")
-      .from("dataset1", "dataset2")
-      .select("field1","field2","field3")
-      .where()
+      create connection() to("hdfs://192.2.2.1/test")
+      from("dataset1", "dataset2")
+      select("field1","field2","field3")
+      where()
     )
   }
 
