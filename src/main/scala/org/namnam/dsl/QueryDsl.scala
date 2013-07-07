a9cd3292194a3c19fc1c304cd5df8ef25da0dbe5
Bernd Zuther <bernd.zuther@comsysto.com>
2013-07-07 18:14:54 +0200

    extends dsl a little bit

--- src/main/scala/org/namnam/dsl/QueryDsl.scala
+++ src/main/scala/org/namnam/dsl/QueryDsl.scala
@@ -3,15 +3,59 @@ package org.namnam.dsl
 /**
  * @author zutherb
  */
-case class Fields(fields:Seq[String])
+case class Fields(val fields:Seq[String])
+case class Datasets(val datasets:Seq[String])
+case class Connection(val url:String)
+case class Criterion()
 
-case class Datasets(datasets:Seq[String]){
-  implicit def select(fields:String*):Fields = new Fields(fields)
-}
-case class Connection(url:String){
-  implicit def from(datasets:String*):Datasets = new Datasets(datasets)
+case class ConnectionIdentifier(queryBuilder:QueryBuilder) {
+  private var connection:Option[Connection] = None
+
+  def to(url : String) : QueryBuilder = {
+    this.connection = Some(Connection(url))
+    queryBuilder
+  }
 }
 
-class QueryBuilder {
-  implicit def connect(url:String):Connection = new Connection(url)
+
+class QueryBuilder() {
+  var fields:Option[Fields] = None
+  var dataSets:Option[Datasets] = None
+  var connectionIdentifier:Option[ConnectionIdentifier] = None
+
+  def create () : QueryBuilder = {
+    this
+  }
+
+  def connection (): ConnectionIdentifier = {
+    connectionIdentifier = Some(ConnectionIdentifier(this))
+    connectionIdentifier.get
+  }
+
+
+
+  def from(datasets:String*):QueryBuilder = {
+    this.dataSets = Some(Datasets(datasets))
+    this
+  }
+
+  def select(fields:String*):QueryBuilder = {
+    this.fields = Some(Fields(fields))
+    this
+  }
+
+  def where(Criterion:Criterion*) = {
+    this
+  }
+
+  override def toString: String = {
+    val url: String = "mongodb://localhost/test"
+    s"""
+        create  connection to \"$url\"
+        from    collection1, collection2
+        where   collection1.a.b = 123 AND
+                collection2.b.c = 123
+        select  collection1.a, collection2.b
+    """
+  }
 }
--- src/test/scala/org/namnam/dsl/QueryExamples.scala
+++ src/test/scala/org/namnam/dsl/QueryExamples.scala
@@ -2,8 +2,13 @@ package org.namnam.dsl
 
 object QueryExamples extends QueryBuilder {
 
-  connect("hdfs://192.2.2.1/test")
-    .from("dataset1", "dataset2")
-    .select("field1","field2","field3")
+  def main(args: Array[String]) {
+    println(
+      create().connection().to("hdfs://192.2.2.1/test")
+      .from("dataset1", "dataset2")
+      .select("field1","field2","field3")
+      .where()
+    )
+  }
 
 }
